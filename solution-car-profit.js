d7710832cc7c624bdaa065d925fd4d8d70744b91
Georgi Pavlov <georgipavlov17@gmail.com>
2022-11-26 21:35:41 +0200

    solution

---
+++ solution-car-profit.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", carProfit);
+
+function carProfit() {
+    
+    const input = {
+        make: document.getElementById('make'),
+        model: document.getElementById('model'),
+        year: document.getElementById('year'),
+        fuel: document.getElementById('fuel'),
+        originalPrice: document.getElementById('original-cost'),
+        sellingPrice: document.getElementById('selling-price')
+    };
+
+    const lists = {
+        sellingOfferList: document.getElementById('table-body'),
+        publishList: document.getElementById('cars-list')
+    };
+
+    const publishBtn = document.getElementById('publish');
+    publishBtn.addEventListener("click", publish);
+
+    function publish(ev) {
+        ev.preventDefault();
+
+        let carMake = input.make.value;
+        let carModel = input.model.value;
+        let carYear = input.year.value;
+        let carFuel = input.fuel.value;
+        let carOriginalPrice = input.originalPrice.value;
+        let carSellingPrice = input.sellingPrice.value;
+
+        if(carMake === '' || carModel === '' || carYear === '' || carFuel === '' || carOriginalPrice === '' || carSellingPrice === '') {
+            return;
+        }
+    }
+}
